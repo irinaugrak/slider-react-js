6c7ea77fe45a5eb9e0501938bf23f7e90a0486ca
irinaugrak <irina.ugrak@gmail.com>
2019-01-14 23:04:33 +0300

    Создание приложение. Урок 2. Все основные компоненты, кроме слайдера

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
+
+const url = process.env.PUBLIC_URL + '/img/bg.jpg'
+
+const HeaderWrapper = styled.header`
+	width: 100%;
+	height: 49px;
+	background-color: #242424;
+	opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+	height: 89px;
+	padding-top: 20px;
+`
+
+const MainWrapper = styled.main`
+	height: 600px;
+	paddgin-top: 130px;
+	background: url(${url}) no-repeat;
+	background-size: cover;
+`
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <SayFullName name="Ivan" surnname="Ivanov" link="vk.com" />
-        <SayFullName name="Anna" surnname="Sidorova" link="vk.com" />
-        <SayFullName name="Katya" surnname="Petrova" link="vk.com" />
-    </div>
-    );
+      <div className="App">
+      	<HeaderWrapper>
+	      	<Grid>
+	      		<Header />
+	      	</Grid>
+      	</HeaderWrapper>
+      	<MenuWrapper>
+	      	<Grid>
+	      		<Menu />
+	      	</Grid>
+      	</MenuWrapper>
+    	<MainWrapper>
+	      	<Grid>
+	      		<Main />
+	      	</Grid>
+      	</MainWrapper>
+      </div>
+    )
   }
 }
-
-function SayFullName(props) {
-  return (
-    <div>
-      <h1>Мое имя - {props.name}, моя фамилия - {props.surnname}</h1>
-      <a href={props.link}>Ссылка на мой профиль </a>
-    </div>
-    )
-}
-
 
 export default App;
